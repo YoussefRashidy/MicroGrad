41c5d02e940e98808e9acc0ca37e8c934b5a39cb
Youssef Ali Rashidi Ali <es.yousef.rashidy2023@alexu.edu.eg>
2026-09-04 16:51:46 +0300

    Add PermuteInput data class and implement Permute function with forward and backward methods

--- src/main/kotlin/function/FunctionInput.kt
+++ src/main/kotlin/function/FunctionInput.kt
@@ -5,4 +5,5 @@ import io.github.youssefrashidy.tensor.Tensor
 interface FunctionInput {
     data class Tensors(val tensors: Array<Tensor>) : FunctionInput{}
     data class ReshapeInput(val tensors: Array<Tensor> , val shape : IntArray) : FunctionInput{}
+    data class PermuteInput(val tensors: Array<Tensor>, val permute: IntArray) : FunctionInput{}
 }
---
+++ src/main/kotlin/function/structural/Permute.kt
@@ -0,0 +1,75 @@
+package io.github.youssefrashidy.function.structural
+
+import io.github.youssefrashidy.function.Function
+import io.github.youssefrashidy.function.FunctionInput
+import io.github.youssefrashidy.tensor.Tensor
+
+class Permute : Function() {
+    override fun forward(input: FunctionInput): Tensor {
+        require(input is FunctionInput.PermuteInput){
+
+        }
+        require(input.tensors.size == 1){
+
+        }
+        val inputTensor = input.tensors[0]
+        val permutation = input.permute
+        require(permutation.size == inputTensor.rank){
+            "Permutation must have ${inputTensor.shape.size} dimensions, but got ${permutation.size}"
+        }
+        require(permutation.toSet().size == permutation.size && permutation.all { it in inputTensor.shape.indices }) {
+            "Invalid permutation: ${permutation.contentToString()}"
+        }
+        val outputTensor = permute(inputTensor,permutation)
+        outputTensor.prevTensors = arrayOf(inputTensor)
+        backward(inputTensor, outputTensor,permutation = permutation)
+        return outputTensor
+    }
+
+    // I will address this later
+    override fun backward(vararg tensors: Tensor) {
+        val inputTensor = tensors[0]
+        val outputTensor = tensors[1]
+        if(inputTensor.grad== null)
+            inputTensor.grad = Tensor(DoubleArray(inputTensor.size),inputTensor.shape,false,inputTensor.strides)
+        if(inputTensor.requiresGrad){
+            outputTensor.gradFn = {
+
+            }
+        }
+    }
+
+    private fun backward(vararg tensors: Tensor , permutation: IntArray) {
+        val inputTensor = tensors[0]
+        val outputTensor = tensors[1]
+        val inversePermute = inversePermutation(permutation)
+        if(inputTensor.grad== null)
+            inputTensor.grad = Tensor(DoubleArray(inputTensor.size),inputTensor.shape,false,inputTensor.strides)
+        if(inputTensor.requiresGrad){
+            outputTensor.gradFn = {
+                accumulateAddition(inputTensor.grad!!,permute(outputTensor.grad!!,inversePermute))
+            }
+        }
+    }
+
+    private fun permute(tensor: Tensor , permutation : IntArray) : Tensor{
+        val newShape = IntArray(tensor.rank)
+        val newStride = IntArray(tensor.rank)
+        for(i in permutation.indices){
+            newShape[i] = tensor.shape[permutation[i]]
+            newStride[i] = tensor.strides[permutation[i]]
+        }
+        val outputTensor = Tensor(tensor.backedArray,newShape,tensor.requiresGrad,newStride)
+        return outputTensor
+    }
+
+    private fun inversePermutation(permutation: IntArray): IntArray {
+        val inverse = IntArray(permutation.size)
+
+        for (i in permutation.indices) {
+            inverse[permutation[i]] = i
+        }
+
+        return inverse
+    }
+}
